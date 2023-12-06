7db63f05399098924b9cfae9acec0efc727a43e0
ryabubaker <ryabubaker@gmail.com>
2023-12-07 00:59:42 +0200

    Upload images to database

--- encode.py
+++ encode.py
@@ -2,6 +2,17 @@ import os
 import cv2
 import face_recognition
 import pickle
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : "https://facerecognition-159b3-default-rtdb.firebaseio.com/",
+    'storageBucket' : "facerecognition-159b3.appspot.com"
+})
+
 folderpath = 'Datasets'
 pathlist = os.listdir(folderpath)
 print(pathlist)
@@ -11,6 +22,12 @@ studIds=[]
 for path in pathlist:
  img_list.append(cv2.imread(os.path.join(folderpath,path)))
  studIds.append(os.path.splitext(path)[0])
+ 
+ fileName = f'{folderpath}/{path}'
+ bucket = storage.bucket()
+ blob = bucket.blob(fileName)
+ blob.upload_from_filename(fileName)
+
 print(studIds)
 
 def findencode(imagelist):
